e73ab2ff72d7102f564d85777d2d3380501fd929
Raiyan-sharif <raiyan.sharif.1234@gmail.com>
2026-03-04 15:53:26 +0600

    Hiya App Project init

---
+++ Hiya-App/Students/HiyaApp/HiyaApp/HiyaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaAppApp.swift
+//  HiyaApp
+//
+//  Created by Raiyan Sharif on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HiyaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
