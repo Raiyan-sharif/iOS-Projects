a0c5b5e93bcf49e5c941b9d546100259fc0d45e5
Raiyan-sharif <raiyan.sharif.1234@gmail.com>
2026-03-09 14:36:42 +0600

    updated

--- Hiya-App/Students/HiyaApp/HiyaApp/ContentView.swift
+++ Hiya-App/Students/HiyaApp/HiyaApp/ContentView.swift
@@ -10,12 +10,34 @@ import FoundationModels
 
 struct ContentView: View {
     private var largeLanguageModel = SystemLanguageModel.default
+    private var session = LanguageModelSession()
+    @State private var response: String = ""
+    @State private var isLoading: Bool = false
     
     var body: some View {
         VStack {
+            Spacer()
             switch largeLanguageModel.availability {
                 case .available:
-                Text("Large Language Model is available!")
+                if response.isEmpty {
+                    if isLoading {
+                        ProgressView()
+                    } else {
+                        Text("Tap the button to get a response")
+                            .foregroundStyle(.tertiary)
+                            .multilineTextAlignment(.center)
+                            .font(.title)
+                        
+                    }
+                    
+                } else {
+                    
+                    
+                    Text("\(response)")
+                        .multilineTextAlignment(.center)
+                        .font(.largeTitle)
+                        .bold()
+                }
             case .unavailable(.deviceNotEligible):
                 Text("Large Language Model is unavailable because device is not eligible")
             case .unavailable(.appleIntelligenceNotEnabled):
@@ -25,8 +47,35 @@ struct ContentView: View {
             case .unavailable(_):
                 Text("AL model is not available reason unknown")
             }
+            
+            Spacer()
+            
+            Button{
+                Task {
+                    isLoading = true
+                    defer {
+                        isLoading = false
+                    }
+                    let prompt = "Hello, how are you today?"
+                    do{
+                        let reply = try await session.respond(to: prompt)
+                        response = reply.content
+                    } catch {
+                        response = "Failed to get response: \(error.localizedDescription)"
+                    }
+                }
+                
+            } label: {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonSizing(.flexible)
+            .glassEffect(.regular.interactive())
         }
         .padding()
+        .tint(.purple)
     }
 }
 
